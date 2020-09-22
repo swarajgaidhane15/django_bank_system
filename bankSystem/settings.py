c2863d931d28a6659f03dbe74f196dbc18c0326d
swarajgaidhane15 <rutvikgaidhane1508@gmail.com>
2020-09-22 12:46:17 +0530

    Ready to deploy

--- bankSystem/settings.py
+++ bankSystem/settings.py
@@ -22,28 +22,28 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '8*(!x3%ma8zf_))z_=s79k%1gpclz@umjjnxjtwdj$6-&d2pqz'
-SECRET_KEY = os.environ.get('SECRET_KEY_BANK')
+SECRET_KEY = '8*(!x3%ma8zf_))z_=s79k%1gpclz@umjjnxjtwdj$6-&d2pqz'
+# SECRET_KEY = os.environ.get('SECRET_KEY_BANK')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    'my-bankmanager-app.herokuapp.com'
+    'my-bankmanager-app.herokuapp.com',
+    'localhost'
 ]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    'bank',
+    'crispy_forms', 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bank',
-    'crispy_forms',
 ]
 
 MIDDLEWARE = [
